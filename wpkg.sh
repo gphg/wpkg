e325e17c458e128f1186f2784d0150daf72f0041
Garett PHG <suffering.train@yahoo.com>
2024-09-04 23:33:37 +0700

    Unfinished: tab instead space + hash specs.

--- wpkg.sh
+++ wpkg.sh
@@ -13,32 +13,51 @@ echo "[INFO] Downloading packages listed in:" $WGET_PACKAGES_FILE
 TEMPFILE=$(mktemp)
 grep -v -e"^$" -e"^\s*#" $WGET_PACKAGES_FILE > $TEMPFILE
 
+__verify() {
+    local _file="$1"
+    local _func=$(echo "$2" | cut -d':' -f1)
+    case $_func in
+        md5) md5sum $_file
+            ;;
+        sha1) sha1sum $_file
+            ;;
+        sha224) sha224sum $_file
+            ;;
+        sha256) sha256sum $_file
+            ;;
+        sha384) sha384sum $_file
+            ;;
+        sha512) sha512sum $_file
+            ;;
+        *) return 1
+            ;;
+    esac
+    return 0
+}
+
 # per line: download file to target path and check sha1 checksum
-while read LINE
+while read -r URL FILE HASH
 do
-    URL=$(echo "$LINE" | cut -d' ' -f1)
-    FILE=$(echo "$LINE" | cut -d' ' -f2)
-    SHA1=$(echo "$LINE" | cut -d' ' -f3)
+    HASH_SUM=$(echo "$HASH" | cut -d':' -f2)
 
     # validation (haha)
     if [ $URL = $FILE ] ; then
         echo "[ERROR] Invalid wget download instruction. URL and filename are the same:" $LINE
         continue
     fi
-    if [ $FILE = $SHA1 ] ; then
+    if [ $FILE = $HASH ] ; then
         echo "[ERROR] Invalid wget download instruction. Filename and checksum are the same:" $LINE
         continue
     fi
-    if [ $URL = $SHA1 ] ; then
+    if [ $URL = $HASH ] ; then
         echo "[ERROR] Invalid wget download instruction. Sha1 value the same as the URL:" $LINE
         continue
     fi
 
     # check if local file already exists and has the correct checksum
     if [ -f $FILE ] ; then
-        CHECKSUM=$(sha1sum $FILE)
-        CHECKSUM=$(echo "$CHECKSUM" | cut -d' ' -f1)
-        if [ "$CHECKSUM" = "$SHA1" ] ; then
+        CHECKSUM=$(__verify "$FILE" "$HASH" | cut -d' ' -f1)
+        if [ "$CHECKSUM" = "$HASH_SUM" ] ; then
             echo "[SUCCESS] File already downloaded correctly:" $FILE
             continue
         else
@@ -54,16 +73,14 @@ do
 
     # verify downloaded file
     echo "[INFO] Calculating checksum for file:" $FILE
-    CHECKSUM=$(sha1sum $FILE)
-    CHECKSUM=$(echo "$CHECKSUM" | cut -d' ' -f1)
-    if [ "$CHECKSUM" = "$SHA1" ] ; then
+    CHECKSUM=$(__verify "$FILE" "$HASH" | cut -d' ' -f1)
+    if [ "$CHECKSUM" = "$HASH_SUM" ] ; then
         echo "[SUCCESS] Download succesful:" $FILE
     else
         echo "[ERROR] Checksum error for downloaded file:" $FILE
-        echo "[ERROR] Expected   =>" $SHA1
+        echo "[ERROR] Expected   =>" $HASH_SUM
         echo "[ERROR] Calculated =>" $CHECKSUM
     fi
 done < $TEMPFILE
 
 rm $TEMPFILE
-
